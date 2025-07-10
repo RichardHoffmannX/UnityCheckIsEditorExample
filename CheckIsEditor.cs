a1efcbd3a9d11059bb992d35834f44651d50e22c
Richard Hoffmann <95046844+RichardHoffmannX@users.noreply.github.com>
2025-07-10 23:28:39 +0200

    Add files via upload

---
+++ CheckIsEditor.cs
@@ -0,0 +1,32 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CheckIsEditor : MonoBehaviour
+{
+    public bool isEditor = false;
+    public bool unityEditor = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (Application.isEditor)
+        {
+            isEditor = true;
+        }
+        else
+        {
+            isEditor = false;
+        }
+#if UNITY_EDITOR
+        unityEditor = true;
+#else
+    UNITY_EDITOR = false;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
